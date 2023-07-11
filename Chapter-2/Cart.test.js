84074364409716a3298aed6b6539ff7676685f18
Jakov <j.jakovu@gmail.com>
2023-07-11 10:24:43 +0300

    Add second test

--- Chapter-2/Cart.test.js
+++ Chapter-2/Cart.test.js
@@ -1,11 +1,27 @@
 // @ts-nocheck
 const { db, closeConnection } = require("./dbConnection")
-const { createCart } = require("./cart");
+const { createCart, addItem } = require("./cart");
+
+beforeEach(async () => {
+  await db("carts").truncate();
+  await db("carts_items").truncate();
+});
+afterAll(async () => await closeConnection());
 
 test("createCart creates a cart fro a username", async () => {
-  await db("carts").truncate();
   await createCart("Lucas da Costa");
   const result = await db.select("username").from("carts");
-  expect(result).toEqual([{ username: "Lucas da Costa"}]);
-  await closeConnection();
+  expect(result).toEqual([{ username: "Lucas da Costa" }]);
+});
+
+test("addItem adds an item to a cart", async () => {
+  const username = "Lucas da Costa";
+  await createCart(username);
+  const { id: cartId } = await db
+                                .select()
+                                .from("carts")
+                                .where({ username });
+  await addItem(cartId, "cheesecake");
+  const result = await db.select("itemName").from("carts_items");
+  expect(result).toEqual([{ cartId, itemName: "cheesecake" }]);
 });
