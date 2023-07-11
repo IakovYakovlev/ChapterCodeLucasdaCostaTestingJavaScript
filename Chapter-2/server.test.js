e28a42f06e18acedef91d8eb05bedf12c23c7c9f
Jakov <j.jakovu@gmail.com>
2023-07-11 13:01:37 +0300

    Add koa, koa-router, isomorphic-fetch. And did first test with the server.

---
+++ Chapter-2/server.test.js
@@ -0,0 +1,29 @@
+// @ts-nocheck
+const fetch = require("isomorphic-fetch");
+const app = require("./server");
+
+const apiRoot = "http://localhost:3000";
+
+const addItem = (username, item) => {
+  return fetch(`${apiRoot}/carts/${username}/items/${item}`, {
+    method: "POST"
+  });
+};
+
+const getItems = username => {
+  return fetch(`${apiRoot}/carts/${username}/items`, {
+    method: "GET"
+  });
+};
+
+test("adding items to a cart", async () => {
+  const initialItemsResponse = await getItems("lucas");
+  expect(initialItemsResponse.status).toEqual(404);
+
+  const addItemResponse = await addItem("lucas", "cheesecake");
+  expect(await addItemResponse.json()).toEqual(["cheesecake"]);
+
+  const finalItemsResponse = await getItems("lucas");
+  expect(await finalItemsResponse.json()).toEqual(["cheesecake"]);
+});
+afterAll(() => app.close());
